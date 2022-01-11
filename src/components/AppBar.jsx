0cc6c8c27ceb6e8386b99445ecaf4989fd67c95f
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-01-11 15:59:21 +0200

    Scrollable AppBar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
 import Text from './Text'
@@ -21,16 +21,18 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
   <View style={styles.container}>
-    <View style={styles.flexItem}>
-      <Link to="/">
-        <Text color="textTab">Repositories</Text>
-      </Link>
-    </View>
-    <View style={styles.flexItem}>
-      <Link to="/signin">
-        <Text color="textTab">Sign In</Text>
-      </Link>
-    </View>
+    <ScrollView horizontal style={styles.flexItem}>
+      <View style={styles.flexItem}>
+        <Link to="/">
+          <Text color="textTab">Repositories</Text>
+        </Link>
+      </View>
+      <View style={styles.flexItem}>
+        <Link to="/signin">
+          <Text color="textTab">Sign In</Text>
+        </Link>
+      </View>
+    </ScrollView>
   </View>
   );
 };
