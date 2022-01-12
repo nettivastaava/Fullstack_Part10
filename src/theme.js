0917e3b723d09b8973b732a65be80603556ac4b0
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-01-12 19:24:51 +0200

    Platform-specific fonts

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -9,9 +11,11 @@ const theme = {
     body: 14,
     subheading: 16,
   },
-  fonts: {
-    main: 'System',
-  },
+  fonts: Platform.select({
+    android: 'Roboto',
+    ios: 'Arial',
+    default: 'System',
+  }),
   fontWeights: {
     normal: '400',
     bold: '700',
