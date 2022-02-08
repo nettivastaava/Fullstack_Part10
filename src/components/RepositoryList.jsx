d9b54c22d01846ddcd0232699945278bed88cb1f
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-02-08 08:59:00 +0200

    Picker added

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
+import { Picker } from '@react-native-picker/picker';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  picker: {
+    padding: 15
+  }
 });
 
 const RepositoryList = () => {
   const { repositories } = useRepositories();
+  const [order, setOrder] = useState();
 
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
@@ -22,6 +27,20 @@ const RepositoryList = () => {
     <FlatList
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
+      ListHeaderComponent={() => 
+        <View style={styles.picker}
+        >
+          <Picker
+            selectedValue={order}
+            onValueChange={(itemValue, itemIndex) =>
+              setOrder(itemValue)
+            }>
+            <Picker.Item label="Latest repositories" value="CREATED AT" />
+            <Picker.Item label="Highest rated repositories" value="DESC" />
+            <Picker.Item label="Lowest rated repositories" value="ASC" />
+          </Picker>
+        </View>
+      }
       renderItem={({ item, index, separators }) => (
         <View style={{ backgroundColor: 'white' }}>
           <RepositoryItem repository={item}/>
