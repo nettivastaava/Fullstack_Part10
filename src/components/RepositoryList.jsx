1ef602f788d0b50bdd7a3b6eac66cee0c2296386
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-02-09 21:41:15 +0200

    Filter prototype

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,6 +3,8 @@ import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import { Picker } from '@react-native-picker/picker';
+import TextInput from './TextInput';
+import { useDebounce } from 'use-debounce';
 
 const styles = StyleSheet.create({
   separator: {
@@ -10,6 +12,12 @@ const styles = StyleSheet.create({
   },
   picker: {
     padding: 15
+  },
+  inputField: {
+    borderWidth: 2,
+    borderColor: '#586069',
+    padding: 5,
+    margin: 5
   }
 });
 
@@ -17,7 +25,11 @@ const RepositoryList = () => {
   const [orderBy, setOrderBy] = useState('CREATED_AT');
   const [orderDirection, setOrderDirection] = useState('DESC');
   const [pickedValue, setPickedValue] = useState('Latest');
-  const { repositories } = useRepositories({ orderBy, orderDirection});
+  const [filter, setFilter] = useState('');
+  const [searchKeyword] = useDebounce(filter, 5000);
+  const { repositories } = useRepositories({ orderBy, orderDirection, searchKeyword});
+
+  console.log('FILTER ', filter);
 
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
@@ -46,6 +58,12 @@ const RepositoryList = () => {
       ItemSeparatorComponent={ItemSeparator}
       ListHeaderComponent={() => 
         <View style={styles.picker}>
+          <TextInput 
+            style={styles.inputField} 
+            name="filter" 
+            placeholder="Search"
+            onChangeText={setFilter}
+          />
           <Picker
             selectedValue={pickedValue}
             onValueChange={(itemValue, itemIndex) =>         
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,16 +2,11 @@ import { useQuery } from '@apollo/client';
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = ({ orderBy, orderDirection }) => {
+const useRepositories = ({ orderBy, orderDirection, searchKeyword }) => {
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
-    variables: { orderBy: orderBy, orderDirection: orderDirection } 
+    variables: { orderBy: orderBy, orderDirection: orderDirection, searchKeyword: searchKeyword } 
   });
-
-  console.log('ORDER BY ', orderBy);
-  console.log('ORDER DIR ', orderDirection);
-
-
 
   return { 
     repositories: data ? data.repositories : undefined,
